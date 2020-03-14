de31123f5e7c3601a8ae27ffb85025e5ddc0daef
Kishan <kishan.o@hotmail.com>
2020-03-14 23:46:56 +0000

    Server setup

---
+++ server/static/react/precache-manifest.74d2341a45d85f2688b78b9b8a26044b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2832579a5a0adde8e805f71597bc9a21",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "d4fd237806bacc620182",
+    "url": "/static/react/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92fa15e3a7ac7564114d",
+    "url": "/static/react/css/main.98628838.chunk.css"
+  },
+  {
+    "revision": "d4fd237806bacc620182",
+    "url": "/static/react/js/2.2c93f009.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/react/js/2.2c93f009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92fa15e3a7ac7564114d",
+    "url": "/static/react/js/main.97653b3c.chunk.js"
+  },
+  {
+    "revision": "fe19c3bd0c3c073c1c63",
+    "url": "/static/react/js/runtime-main.2750f46f.js"
+  }
+]);
