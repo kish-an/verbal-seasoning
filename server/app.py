7fbeb3cf879a4b06310027882d13e84b3698101d
shish-kabobs <Kbobyn1@sheffield.Ac.Uk>
2020-03-15 04:11:27 +0000

    move BCI code to server

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify, make_response
+import asyncio
+from cortex import Cortex
 
 app = Flask(__name__)
 
@@ -20,5 +22,37 @@ def robot_talk():
     except:
         res = make_response(jsonify({"message": "SERVER ERROR"}), 500)
         return res
+
+#############
+
+@app.route("/brainstream", methods = ["POST"])
+def read_bci():
+    try:
+        goooo()
+    except:
+        print("no")
+
+def do_brain_stuff:
+    # await cortex.inspectApi()
+    print("** USER LOGIN **")
+    await cort.get_user_login()
+    print("** GET CORTEX INFO **")
+    await cort.get_cortex_info()
+    print("** HAS ACCESS RIGHT **")
+    await cort.has_access_right()
+    print("** REQUEST ACCESS **")
+    await cort.request_access()
+    print("** AUTHORIZE **")
+    await cort.authorize()
+    print("** GET LICENSE INFO **")
+    await cort.get_license_info()
+    print("** QUERY HEADSETS **")
+    await cortex.query_headsets()
+
+def goooo():
+    print("starting")
+    cort = Cortex('credentials.txt')
+    asyncio.run(do_brain_stuff(cort))
+    cort.close()
 
 app.run(debug=True)
